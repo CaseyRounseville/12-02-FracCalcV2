1d077aa3f726fb25a013ad1531454301bd8bb21e
CaseyRounseville <mariarounseville@cox.net>
2014-12-02 23:34:40 -0700

    Update FracCalcV2.java

--- src/FracCalcV2.java
+++ src/FracCalcV2.java
@@ -11,3 +11,25 @@
 //     -- print frac1
 //   - divide frac1 by frac2
 //     -- print frac1
+//Casey Rounseville
+//9O
+//12-2-2014
+public class FracCalcV2
+{
+  public static void main(String[] args)
+  {
+    RationalNumber frac1=new RationalNumber(3,6);
+    RationalNumber frac2=new RationalNumber(2,8);
+    System.out.println("frac1: "+frac1.toString());
+    System.out.println("frac2: "+frac2.toString());
+    frac1.add(frac2);
+    System.out.println("frac1 + frac2: "+frac1);
+    frac1.subtract(frac2);
+    System.out.println("frac1 - frac2: "+frac1);
+    frac1.multiply(frac2);
+    System.out.println("frac1 * frac2: "+frac1);
+    frac1.divide(frac2);
+    System.out.println("frac1 / frac2: "+frac1);
+  }
+}
+  
